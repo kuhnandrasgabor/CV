13f293a5e173ab8cebaad12da9b8fc571652dc7f
András Kühn <eyewitness4560@gmail.com>
2025-02-13 15:37:43 +0100

    Cherry pick gpt resource generator without the actual texts

--- GPT-data/compile-gpt-files.py
+++ GPT-data/compile-gpt-files.py
@@ -1,22 +1,26 @@
 import os
 import shutil
-from combine import generate_pdf_from_md
+
+from scripts.combine import generate_pdf_from_md
 
 
 def compile_gpt_files():
+
+    print("Compiling GPT files...")
+
     # delete the generated directory if it exists
-    if os.path.exists("../GPT-data/generated/"):
-        shutil.rmtree("../GPT-data/generated/")
+    if os.path.exists("generated/"):
+        shutil.rmtree("generated/")
 
     # Create a directory to store the thoughts
-    os.makedirs("../GPT-data/generated/", exist_ok=True)
+    os.makedirs("generated/", exist_ok=True)
 
-    values_filepath = f"../GPT-data/values.md"
-    readme_writings_filepath = f"../GPT-data/readme-writings.md"
-    cv_writings_filepath = f"../GPT-data/cv-writings.md"
+    values_filepath = f"values.md"
+    readme_writings_filepath = f"readme-writings.md"
+    cv_writings_filepath = f"cv-writings.md"
 
-    structured_thoughts_filepath = f"../GPT-data/generated/structured-thoughts.md"
-    writing_styles_samples_filepath = f"../GPT-data/generated/writing-styles-samples.md"
+    structured_thoughts_filepath = f"generated/structured-thoughts.md"
+    writing_styles_samples_filepath = f"generated/writing-styles-samples.md"
 
     # find every line of of the source file that hast the indented "- Thoughts:" in it and remove it, and put the rest into the structured thoughts file
     with open(values_filepath, "r") as values_file:
@@ -30,19 +34,19 @@ def compile_gpt_files():
 
     with open(writing_styles_samples_filepath, "w") as writing_styles_samples_file:
 
-        with open(cv_writings_filepath, "r") as cv_writings_file:
-            for line in cv_writings_file:
+        with open(values_filepath, "r") as values_file:
+            for line in values_file:
                 writing_styles_samples_file.write(line)
 
         with open(readme_writings_filepath, "r") as readme_writings_file:
             for line in readme_writings_file:
                 writing_styles_samples_file.write(line)
 
-        with open(values_filepath, "r") as values_file:
-            for line in values_file:
+        with open(cv_writings_filepath, "r") as cv_writings_file:
+            for line in cv_writings_file:
                 writing_styles_samples_file.write(line)
 
-    print("GPT files compiled successfully!")
+    print("GPT MD files compiled successfully!")
 
     # generate pdf from the markdown files
     generate_pdf_from_md(structured_thoughts_filepath, structured_thoughts_filepath.replace(".md", ".pdf"), "https://github.com/kuhnandrasgabor/cv")
